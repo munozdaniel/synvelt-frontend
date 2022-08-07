0f1b350b6f5ebae569276fc54ddce1b32e3ebb10
Munoz Daniel <dmunoz@micracel.com.ar>
2022-08-07 11:12:06 -0300

    menu

--- src/app/core/navigation/navigation-menu.ts
+++ src/app/core/navigation/navigation-menu.ts
@@ -10,9 +10,44 @@ export const horizontalNavigation: SynveltNavigationItem[] = [
     link: '/inicio',
   },
   {
-    id: 'usuario',
-    title: 'Usuario',
+    id: 'extra',
+    title: 'Opción 1',
     type: 'basic',
-    link: '/usuarios',
+    link: '/inicio',
+  },
+  {
+    id: 'extra',
+    title: 'Opción 2',
+    type: 'basic',
+    link: '/inicio',
+  },
+  {
+    id: 'extra',
+    title: 'Opción 3',
+    type: 'basic',
+    link: '/inicio',
+  },
+  {
+    id: 'administrador.parametros',
+    title: 'Configuración',
+    // subtitle: 'Configurar datos del sistema',
+    type: 'collapsable',
+    // icon: 'heroicons_outline:cog',
+    children: [
+      {
+        id: 'usuario',
+        title: 'Usuario',
+        type: 'basic',
+        link: '/usuarios',
+        icon: 'heroicons_outline:cog',
+      },
+      {
+        id: 'rol',
+        title: 'Roles',
+        type: 'basic',
+        link: '/roles',
+        icon: 'heroicons_outline:cog',
+      },
+    ],
   },
 ];
