6561d02f676e911adf0f1bef60d92516fe1aa2d8
Munoz Daniel <dmunoz@micracel.com.ar>
2022-09-13 17:27:50 -0300

    menu + home

--- src/app/modules/admin/example/example.module.ts
+++ src/app/modules/admin/example/example.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { Route, RouterModule } from '@angular/router';
 import { ExampleComponent } from 'app/modules/admin/example/example.component';
 
@@ -12,6 +14,11 @@ const exampleRoutes: Route[] = [
 
 @NgModule({
   declarations: [ExampleComponent],
-  imports: [RouterModule.forChild(exampleRoutes), MatCardModule],
+  imports: [
+    RouterModule.forChild(exampleRoutes),
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+  ],
 })
 export class ExampleModule {}
